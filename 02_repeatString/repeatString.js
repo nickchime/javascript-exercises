1fd0e09fbbcf81ebd8252d4eff88fa9e06bdf880
nickchime <116189348+nickchime@users.noreply.github.com>
2023-01-12 13:54:05 -0500

    1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, number) {
+    let newString = string;
+    if(number<0){
+        newString = "ERROR";
+        return newString;
+    }
+    else if(number==0){
+        newString = "";
+        return newString;   
+    }
+    else{
+    for(let i=1;i<number;i++ ){
+   
+            newString += string;
+    }
+    return newString;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
